2c995315b02ef55b03967efc22e288afffd8c9bb
Alexis Massiaux <alexis.massiaux@worldline.com>
2025-01-26 19:32:07 +0100

    refactor: replace getter by lombok @Getter for PlayerTotalScore

--- src/main/java/back/eva/model/PlayerTotalScore.java
+++ src/main/java/back/eva/model/PlayerTotalScore.java
@@ -1,26 +1,21 @@
 package back.eva.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 
+@Getter
 @Entity
-@Table(name = "player")
+@Table( name = "player" )
 public class PlayerTotalScore {
 	
 	@Id
 	private Long id;
 	
 	private String username;
-
+	
 	private String killDeathRatio;
 	
-	public String getUsername() {
-		return username;
-	}
-	
-	public String getKillDeathRatio() {
-		return killDeathRatio;
-	}
 }
+
